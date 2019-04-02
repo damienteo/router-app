9b8a653028d78a4d30ebbc5995dd83f1c20f9d70
damienteo <ksteo89@gmail.com>
2019-04-02 11:47:55 +0800

    Programmatic navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    this.props.history.push("/products");
   };
-
+  //push adds a new address in the browser history, so you can go back to the previous page. Replace simply replaces the url. Replac eis used for login pages, so users wont be able to go bacl to the login page
   render() {
     console.log(this.props);
     return (
